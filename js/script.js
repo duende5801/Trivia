9a8f6826191d4e0952b9156a0f8a9e7d118df808
Code <code@COCTL-41270.local>
2019-11-06 15:49:00 -0800

    Fifth Commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
-function loadJSON(url) {
-    //load our JSON Data
-    //XML HTTP-REQUEST
-
-    let xmlhttp = new XMLHttpRequest();
-    // let url = "../data/data.json";
-
-    xmlhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let myArr = JSON.parse(this.responseText);
-            loadEZQ(myArr);
-        }
-    };
-    //Opening the connection
-    xmlhttp.open("GET", url, true);
-    //Sending the request
-    xmlhttp.send();
-
-}
-
-//Play easy mode
-//grab easy mode json data
-let playEZBtn = document.getElementById('playEZ');
-playEZBtn.addEventListener('click', function(e){
-    loadJSON("../data/data.json");
-    //loadJSON("../data/datahd.json");
-});
+//Grab inject elements
+let inject = document.getElementById('inject');
+let g2Menu = document.getElementById('g2Menu');
 
 //Grab all the buttons
 let a1 = document.getElementById('a1');
@@ -39,6 +15,59 @@ let disQ = document.getElementById('qL');
 let counter = document.getElementById('counter');
 //random Question
 let count=0;
+
+
+//Inject from TITLE screen to MENU screen
+g2Menu.addEventListener('click', function(e){
+    //call loadJSON to inject HTML
+    loadJSON("../menu_screen.html");
+});
+
+function loadJSON(url) {
+    //load our JSON Data
+    //XML HTTP-REQUEST
+
+    let xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let myArr = this.responseText;//JSON.parse(this.responseText);
+            console.log(myArr);
+            if(url==="../menu_screen.html"){
+                menuScreenLoad(myArr);
+            }
+
+        }
+    };
+    //Opening the connection
+    xmlhttp.open("GET", url, true);
+    //Sending the request
+    xmlhttp.send();
+}
+
+function menuScreenLoad(info){
+    //going to load page 1 html elements and click events
+    inject.innerHTML = info;
+
+    //add in elements and event listeners
+    let playEZBtn = document.getElementById('playEZ');
+    let playMedBtn = document.getElementById('playMed');
+    let playHrdBtn = document.getElementById('playHrd');
+    let optionsScreen = document.getElementById('playOpts');
+
+    playEZBtn.addEventListener('click', function (e){
+
+    });
+    playMedBtn.addEventListener('click', function (e){
+
+    });
+    playHrdBtn.addEventListener('click', function (e){
+
+    });
+    optionsScreen.addEventListener('click', function (e){
+
+    });
+}
 
 function funcRandom(){
     return Math.floor(Math.random()*ezQ.length);
@@ -79,7 +108,6 @@ a4.addEventListener('click', function(){
     checkAnswer(a4.innerText);
     displayQuestion();
 })
-
 function checkAnswer(string){
     if (string === ezQ[count].c){
         score.innerText ++;
